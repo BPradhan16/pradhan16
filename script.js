378eb9e27c228d468e7cd90a0ebf497173502071
BPradhan16 <banchhanidhi16@gmail.com>
2026-09-26 15:32:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -94,3 +94,14 @@ function openReview() {
         <strong>Date:</strong> ${trade.date}
     `;
 }
+let trade = {
+    symbol: "NIFTY",
+    entry: 100,
+    exit: 120,
+    quantity: 50
+};
+
+localStorage.setItem(
+    "trade",
+    JSON.stringify(trade)
+);
